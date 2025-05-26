2f08f019cc6ae9e420394037d5e85de620f5b9b9
frankuc19 <168152573+frankuc19@users.noreply.github.com>
2025-05-26 12:02:12 -0400

    Update 0_💡_Glosario.py

--- Preroute/pages/0_💡_Glosario.py
+++ Preroute/pages/0_💡_Glosario.py
@@ -29,7 +29,7 @@ ICON_MAP = {
     "Shield": """<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path></svg>""",
 }
 
-@st.cache_data(ttl=60)
+@st.cache_data(ttl=600)
 def load_data_from_sheet():
     try:
         scopes = [
